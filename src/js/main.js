6390f799002b170270790ff2946bdda9dd13e4e5
Thomas Koenig <thomas.koenig@sueddeutsche.de>
2017-05-30 22:16:50 +0200

    Image

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
-require('../img/Faces__1.png');
+const img1 = require('../img/Faces__1.png');
+var img = document.createElement('img');
+img.src = img1;
+img.className = 'image';
+
+var body = document.querySelector('body');
+body.appendChild(img);
 
 console.log('hello');
