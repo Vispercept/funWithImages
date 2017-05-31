d37ffe6458a255fa5747a1f6fdc24d13f28aefc2
Thomas Koenig <thomas.koenig@sueddeutsche.de>
2017-05-31 20:43:49 +0200

    Go on

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
+// image should be around 1000px
 const imagePath = require('../img/img.jpg');
-const numberOfSlices = 20;
+const numberOfSlices = 32;
+let gravity = 1;
 
 const wrapper = document.createElement('div');
 wrapper.className = 'sliced-wrapper'
@@ -8,21 +10,51 @@ body.appendChild(wrapper);
 
 const original = document.createElement('img');
 original.src = imagePath;
-original.style.width = '100%';
 original.style.visibility = 'hidden';
-original.style.position = 'absolute';
 wrapper.appendChild(original);
-console.log(original.getBoundingClientRect());
 
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+let slices = [];
 
 for (var index = 0; index < numberOfSlices; index++) {
   const img = document.createElement('div');
-  img.style.left = `${(index)*(100/numberOfSlices)}%`;
-  img.style.width = `${100/numberOfSlices}%`;
-  img.style.height = '100%';
-  img.style.backgroundPositionX = `${(index+1)*(100/numberOfSlices)}%`;
-  img.style.backgroundImage = `url(${imagePath})`;
-  img.className = 'slice';
   wrapper.appendChild(img);
+  slices.push({ img, randomY: random(-40, 40) })
 }
+
+updateSlices();
+
+function updateSlices() {
+  slices.forEach(({ img, randomY }, index) => {
+    img.style.transform = `translate3d(0, ${randomY * gravity}px, 0)`;
+    img.style.left = `${(index) * (100 / numberOfSlices)}%`;
+    img.style.width = `${100 / numberOfSlices}%`;
+    img.style.height = '100%';
+    img.style.backgroundPositionX = `${(index + 1) * (100 / numberOfSlices)}%`;
+    img.style.backgroundPositionY = `${randomY * gravity}px`;
+    img.style.backgroundImage = `url(${imagePath})`;
+    img.className = 'slice';
+  });
+}
+
+
+function update({ clientX }) {
+  gravity = clientX/window.innerWidth;
+  updateSlices();
+}
+
+function keyPressed({keyCode}) {
+  if (keyCode === 32) {
+    slices = slices.map(slice => {
+      slice.randomY = random(-40, 40);
+      return slice;
+    })
+    updateSlices();
+  }
+}
+
+document.addEventListener('mousemove', update)
+document.addEventListener('keyup', keyPressed)
