67fb2a4d5e20f91de625e41224f73a064688ef27
Thomas Koenig <thomas.koenig@sueddeutsche.de>
2017-05-31 16:03:36 +0200

    Progress

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,28 @@
-const body = document.querySelector('body');
 const imagePath = require('../img/img.jpg');
+const numberOfSlices = 20;
 
-const img = document.createElement('img');
-img.src = imagePath;
-img.className = 'image';
-body.appendChild(img);
+const wrapper = document.createElement('div');
+wrapper.className = 'sliced-wrapper'
+const body = document.querySelector('body');
+body.appendChild(wrapper);
 
-const img2 = document.createElement('img');
-img2.src = imagePath;
-img2.className = 'image';
-body.appendChild(img2);
+const original = document.createElement('img');
+original.src = imagePath;
+original.style.width = '100%';
+original.style.visibility = 'hidden';
+original.style.position = 'absolute';
+wrapper.appendChild(original);
+console.log(original.getBoundingClientRect());
 
+
+
+for (var index = 0; index < numberOfSlices; index++) {
+  const img = document.createElement('div');
+  img.style.left = `${(index)*(100/numberOfSlices)}%`;
+  img.style.width = `${100/numberOfSlices}%`;
+  img.style.height = '100%';
+  img.style.backgroundPositionX = `${(index+1)*(100/numberOfSlices)}%`;
+  img.style.backgroundImage = `url(${imagePath})`;
+  img.className = 'slice';
+  wrapper.appendChild(img);
+}
